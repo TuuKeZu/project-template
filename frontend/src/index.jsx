25d1b8c0536ab6293d6aa04c382c11b551f74bd3
TuuKeZu <moilanentuukka5@gmail.com>
2022-03-23 20:51:28 +0200

    Sensor functionality

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
-import {FormControl, InputLabel, Select, MenuItem} from '@mui/material';
+import {FormControl, InputLabel, Select, MenuItem, Button} from '@mui/material';
 
 
 import { Chart as ChartJS } from 'chart.js/auto'
@@ -20,9 +19,10 @@ const options = {
   maintainAspectRatio: false,
 }
 
-// fetch sensor data from backend
-const getData = async () => {
+let sensorList = [];
 
+// fetch sensor data from backend
+const getData = async (sensor_id = String) => {
   // the format in which data is passed to a chart
   const data = {
     datasets: [
@@ -40,14 +40,14 @@ const getData = async () => {
     labels: []
   }
 
-  const res = await fetch(`${backendUrl}/api/events/all`);
+  const res = await fetch(`${backendUrl}/api/events/?sensor_id=${sensor_id}`);
   const { results } = await res.json();
 
   // add the fetched data into the data structure above
   results.forEach(datapoint => {
     data.datasets[0].data.push(datapoint.temperature);
     data.datasets[1].data.push(datapoint.humidity);
-    data.labels.push(datapoint.createdAt);
+    data.labels.push(formatDateTime(datapoint.createdAt));
   });
 
   return data;
@@ -57,34 +57,38 @@ const getSensors = async () => {
   const res = await fetch(`${backendUrl}/api/events/sensors`);
   const { results } = await res.json();
 
-  console.log(results);
+  sensorList = results.filter(result => result != null);
 
-  return results.filter(result => result != null).map((result, index) => 
+  return results.filter(result => result != null).map((result, index) =>
     <MenuItem key={index} value={index}>{result}</MenuItem>
   );
 }
 
 const App = () => {
   const [data, setData] = useState();
-  const [sensorList, setSensorList] = useState([]);
+  const [sensorElementList, setSensorList] = useState([]);
   const [sensor, setSensor] = useState(0);
 
-  const handleChange = (event) => {
+  const handleChange = async (event) => {
     setSensor(event.target.value);
-    
+    setData(await getData(sensorList[event.target.value]));
+  }
+
+  const handleRefresh = async () => {
+    setData(await getData(sensorList[sensor]));
   }
 
   // useEffect will be run only on the first render
   useEffect(async () => {
     // here we get the data from the backend...
-    const chartData = await getData();
-    console.log(chartData);
+    
+    const sensorData = await getSensors();
+    setSensorList(sensorData);
+
+
+    const chartData = await getData(sensorList[0]);
     // ...and store it for later use
     setData(chartData);
-
-    const sensorData = await getSensors();
-    console.log(sensorData);
-    setSensorList(sensorData);
   }, []);
 
   // display loading text until we have fetched the data
@@ -105,12 +109,41 @@ const App = () => {
             onChange={handleChange}
           >
           
-          { sensorList }
+          { sensorElementList }
         </Select>
       </FormControl>
       <Line data={data} opotions={options} />
+      <Button onClick={() => handleRefresh()}>Refresh</Button>
     </div>
   );
+}
+
+const formatDateTime = (raw = string) => {
+  const d = new Date(raw);
+  const WEEKDAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thrusday',
+    'Friday',
+    'Saturday'
+  ]
+
+  return `${WEEKDAYS[d.getDay()]} ${formatAMPM(d)}`;
+}
+
+// https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+
+  return strTime;
 }
 
 ReactDOM.render(
